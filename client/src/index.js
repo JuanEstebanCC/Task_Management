8c48874de6ea6ae8f62d437193ec768ef073e3a5
JuanEstebanCC <juanescifuentes75@gmail.com>
2021-02-12 18:47:36 -0500

    Making screens

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Route exact path='/' />
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +19,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
